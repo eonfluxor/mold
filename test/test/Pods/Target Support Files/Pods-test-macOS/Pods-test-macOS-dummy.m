69244163e0fa23d05d8bed4b4c32aa704c09fe98
hassan uriostegui <hassanvfx@gmail.com>
2018-09-10 12:37:35 -0700

    removed test

--- test/test/Pods/Target Support Files/Pods-test-macOS/Pods-test-macOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_test_macOS : NSObject
-@end
-@implementation PodsDummy_Pods_test_macOS
-@end
--- test/test/Pods/Target Support Files/Pods-test-macOSTests/Pods-test-macOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_test_macOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_test_macOSTests
-@end
